f47b7eddf114d0da0bd9209056bb32687e5e3ef9
GÜL <gulasga96@gmail.com>
2021-01-12 09:25:11 +0300

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Media File |*.mp3";
+            file.InitialDirectory = Application.StartupPath;
+            file.Title = "Dosya seç";
+            file.ShowDialog();
+            textBox1.Text = file.FileName;
+            axWindowsMediaPlayer1.URL = textBox1.Text;
+
+        }
+    }
+}
